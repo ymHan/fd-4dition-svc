e28a4f16484d6e2b58d789d8c21342b6208133fa
ymHan <onyxsard@gmail.com>
2023-12-22 09:34:27 +0900

    [ymhan] 파일 삭제

--- src/events/fdition.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Socket } from 'socket.io';
-import { v4 as uuidv4 } from 'uuid';
-
-@Injectable()
-export class FditionService {
-  createVideo(client: Socket, videoName: string): void {}
-
-}
